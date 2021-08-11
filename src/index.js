79de6901448d4bfd2ebcb81fba6ac5c40abb4335
Erick Hilario León <erprogramador@gmail.com>
2021-08-11 09:54:45 -0500

    feature: create mass search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AppMassSearch from './AppMassSearch';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppMassSearch />
   </React.StrictMode>,
   document.getElementById('root')
 );
